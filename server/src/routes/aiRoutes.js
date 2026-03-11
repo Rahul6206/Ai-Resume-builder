d97e4f60a98f65de9a5ef1d59142c6d6aabac096
Rahul Kumar <rahulrp8677@gmail.com>
2026-03-11 21:34:16 +0530

    Remove log

--- server/src/routes/aiRoutes.js
+++ server/src/routes/aiRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const AiController = require("../controller/aiController");
 const verifyToken = require("../middleware/authMiddleware");
 
-
+ 
 router.post("/generate-resume",verifyToken,AiController.GenerateAiResume );
 
 module.exports=router;
