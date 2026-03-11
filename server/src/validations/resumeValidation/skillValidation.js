fb964e2c4e8bf23b0d9e3fafdb2f6d7828ba1496
Rahul Kumar <rahulrp8677@gmail.com>
2026-03-11 21:34:06 +0530

    change the skillSchema

--- server/src/validations/resumeValidation/skillValidation.js
+++ server/src/validations/resumeValidation/skillValidation.js
@@ -1,8 +1,8 @@
 const { z } = require("zod");
 
-const skillSchema = z
-    .string()
-    .trim()
-    .min(2, "Skill name must be at least 2 characters");
+const skillSchema =  z.string().min(2, "Skill name must be at least 2 characters");
+
 
 module.exports = { skillSchema };
+
+// const interestSchema = z.string().min(2, "Interest name is required");
